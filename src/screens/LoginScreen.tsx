0249da733b087898685c063e09c4692b39dd1dc9
cheeter_Lee <johnson991210@gmail.com>
2023-03-22 23:56:06 +0800

    style login page

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,93 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Text,
+  View,
+  TextInput,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import { LoginScreenStyles } from '../styles/LoginScreenStyles';
+import { LinearGradient } from 'expo-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
 
-export interface ILoginScreenProps {}
+export interface ILoginScreenProps {
+  navigation: any;
+}
 
-const LoginScreen: React.FunctionComponent<ILoginScreenProps> = props => {
+const LoginScreen: React.FunctionComponent<ILoginScreenProps> = ({
+  navigation,
+}) => {
+  const [data, setData] = React.useState({
+    username: '',
+    password: '',
+    check_textInputChange: false,
+    secureTextEntry: true,
+    isValidUser: true,
+    isValidPassword: true,
+  });
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={LoginScreenStyles.container}>
+      <StatusBar backgroundColor="#625B71" barStyle="light-content" />
+      <View style={LoginScreenStyles.header}>
+        <Text style={LoginScreenStyles.text_header}>Welcome back! 😊</Text>
+      </View>
+      <View style={LoginScreenStyles.footer}>
+        <Text style={LoginScreenStyles.text_footer}>邮箱:</Text>
+        <View style={LoginScreenStyles.action}>
+          <FontAwesome name="user-o" color="#333" size={20} />
+          <TextInput
+            placeholder="Email..."
+            style={LoginScreenStyles.textInput}
+            autoCapitalize="none"
+          />
+          <Feather name="check-circle" color="green" size={20} />
+        </View>
+        <Text style={{ ...LoginScreenStyles.text_footer, marginTop: 25 }}>
+          密码:
+        </Text>
+        <View style={LoginScreenStyles.action}>
+          <Feather name="lock" color="#333" size={20} />
+          <TextInput
+            placeholder="Password..."
+            style={LoginScreenStyles.textInput}
+            autoCapitalize="none"
+          />
+          <Feather name="check-circle" color="green" size={20} />
+        </View>
+        <View style={LoginScreenStyles.button}>
+          <TouchableOpacity style={LoginScreenStyles.signIn} onPress={() => {}}>
+            <LinearGradient
+              colors={['#625B71', '#7D5260']}
+              style={LoginScreenStyles.signIn}>
+              <Text style={{ ...LoginScreenStyles.textSign, color: '#fff' }}>
+                登陆
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('SignUpScreen')}
+            style={[
+              LoginScreenStyles.signIn,
+              {
+                borderColor: '#625B71',
+                borderWidth: 1,
+                marginTop: 15,
+              },
+            ]}>
+            <Text
+              style={[
+                LoginScreenStyles.textSign,
+                {
+                  color: '#625B71',
+                },
+              ]}>
+              注册
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
--- src/screens/RootStackScreen.tsx
+++ src/screens/RootStackScreen.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SplashScreen, SignupScreen, LoginScreen } from './';
-import { useNavigation } from '@react-navigation/native';
 
 export interface IRootStackScreenProps {}
 
@@ -10,13 +9,12 @@ const RootStack = createStackNavigator<any>();
 const RootStackScreen: React.FunctionComponent<
   IRootStackScreenProps
 > = props => {
-  const navigation = useNavigation<any>()
 
   return (
     <RootStack.Navigator>
       <RootStack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
-      <RootStack.Screen name="SigninScreen" component={LoginScreen} options={{ headerShown: false }} />
-      <RootStack.Screen name="LoginScreen" component={SignupScreen} options={{ headerShown: false }} />
+      <RootStack.Screen name="LoginScreen" component={LoginScreen} options={{ headerShown: false }} />
+      <RootStack.Screen name="SignupScreen" component={SignupScreen} options={{ headerShown: false }} />
     </RootStack.Navigator>
   );
 };
