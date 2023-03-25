6ddbe8908c9637d50fc26a7fce3c23ce6d74a31b
cheeter_Lee <johnson991210@gmail.com>
2023-03-25 15:02:46 +0800

    hack TS types for bottom tab icons

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-const Tab = createBottomTabNavigator<MainBottomTabScreenProps<any>>();
+// const Tab = createBottomTabNavigator<MainBottomTabScreenProps<any>>();
+const Tab = createBottomTabNavigator<any>();
 
 import routes2 from '../config/routes2';
 
@@ -10,6 +11,7 @@ import { userInfo } from '../values/global';
 
 import { RootStackScreenProps } from '../types';
 import { MainBottomTabScreenProps } from '../types';
+import Ionicons from '@expo/vector-icons/Ionicons'
 type Props = RootStackScreenProps<'Main'>;
 
 export default function MainScreen({ route }: Props) {
@@ -21,7 +23,37 @@ export default function MainScreen({ route }: Props) {
   return (
     <>
       <StatusBar />
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'gallery') {
+              iconName = focused ? 'ios-information-circle' : 'ios-information-circle-outline';
+            }
+            // if (route.name === 'gallery') {
+              //   iconName = focused
+              //     ? 'ios-information-circle'
+            //     : 'ios-information-circle-outline';
+            // } else if (route.name === 'Settings') {
+            //   iconName = focused ? 'ios-list' : 'ios-list-outline';
+            // }
+            if (route.name === 'Gallery') {
+              iconName = 'home-outline'
+            } else if (route.name === 'Mine' ){
+              iconName = 'ios-person-outline'
+            } else if (route.name === 'Upload') {
+              iconName = 'cloud-upload-outline'
+            } else if (route.name === 'Message') {
+              iconName = 'chatbubble-ellipses-outline'
+            } else {
+              iconName = 'settings-outline'
+            }
+            //@ts-ignore
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: '#6750A4',
+          tabBarInactiveTintColor: '#939094',
+        })}>
         {routes2.map((route2: any, i: number) => (
           <Tab.Screen
             key={i}
